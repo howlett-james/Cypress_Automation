f95ae927ff8d6cdca403fdcd30341a1ba4e9aabc
James Howlett <yokeshwararao@hotmail.com>
2021-09-24 18:21:14 +0530

    Implemented Hooks

--- cypress/integration/Tests/amazon.spec.js
+++
@@ -1,7 +0,0 @@
-import LoginPage from '../../Pages/login_page'
-
-describe('Amazon Login Functionality', () => {
-    it('validate Sign In functionality with valid email and password', () => {
-        LoginPage.ValidLogin;
-    });
-});
--- cypress/integration/Tests/amazontest.js
+++ cypress/integration/Tests/amazontest.js
@@ -1,8 +1,11 @@
 import LoginPage from '../../Pages/login_page'
-describe('Amazon login functionality', () => {
-    it('validate login functionality with valid email & Password', () => {
+
+describe('Amazon login functionality', () => {    
+    beforeEach(()=>{
         LoginPage.LaunchURl;
         LoginPage.navigateToSignPage;
+    })
+    it('validate login functionality with valid email & Password', () => {
         LoginPage.EnterEmailID;
         LoginPage.ClickContinueButton;
         LoginPage.EnterPassword;
@@ -10,8 +13,6 @@ describe('Amazon login functionality', () => {
         LoginPage.VerifyAccountProfile;
     });
     it('validate login functionality with valid mobile number & Password', () => {
-        LoginPage.LaunchURl;
-        LoginPage.navigateToSignPage;
         LoginPage.EnterMobileNumber;
         LoginPage.ClickContinueButton;
         LoginPage.EnterPassword;
@@ -19,8 +20,6 @@ describe('Amazon login functionality', () => {
         LoginPage.VerifyAccountProfile;
     });
     it('validate login functionality with valid email & invalid password', () => {
-        LoginPage.LaunchURl;
-        LoginPage.navigateToSignPage;
         LoginPage.EnterEmailID;
         LoginPage.ClickContinueButton;
         LoginPage.EnterInvalidPassword;
@@ -28,8 +27,6 @@ describe('Amazon login functionality', () => {
         LoginPage.VerifyWarningMessage;
     });
     it('validate sign in functionality with valid mobile number and invalid password', () => {
-        LoginPage.LaunchURl;
-        LoginPage.navigateToSignPage;
         LoginPage.EnterMobileNumber;
         LoginPage.ClickContinueButton;
         LoginPage.EnterInvalidPassword;
@@ -37,20 +34,13 @@ describe('Amazon login functionality', () => {
         LoginPage.VerifyErrorMessage;
     });
     it('validate sign in functionality with invalid email', () => {
-        LoginPage.LaunchURl;
-        LoginPage.navigateToSignPage;
         LoginPage.EnterInvalidEmail;
         LoginPage.ClickContinueButton;
         LoginPage.VerifyErrorMessage;
     });
     it('validate sign in functionality with invalid mobile number', () => {
-        LoginPage.LaunchURl;
-        LoginPage.navigateToSignPage;
         LoginPage.EnterInvalidMobileNumber;
         LoginPage.ClickContinueButton
         LoginPage.VerifyPhoneNumberErrorMessage;
-    });
-    it.only('Valid Login', () => {
-        LoginPage.ValidLogin;
     });
 });
